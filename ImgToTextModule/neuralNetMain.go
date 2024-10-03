02e95f8a686f4b71b599d8b0e8555db468605582
not-shoyo <akheelachu@gmail.com>
2024-10-03 12:36:54 +0530

    trained char recognition on MNIST letters plus digits

--- ImgToTextModule/neuralNetMain.go
+++ ImgToTextModule/neuralNetMain.go
@@ -5,8 +5,13 @@ import (
 )
 
 func main() {
-	weightSavingFileName, trainingDataFileName, testingDataFileName := "NeuralNetWeights/testWeightsSaving_3.txt", "MnistTrainingData/emnist-digits-train.csv", "MnistTrainingData/emnist-digits-test.csv"
-	neuralnet.TrainNeuralNetwork(trainingDataFileName, 1, 500, 0.1, -1, -1, -1, 10, 10, weightSavingFileName)
+	// weightSavingFileName, trainingDataFileName, testingDataFileName := "NeuralNetWeights/testWeightsSaving_3.txt", "MnistTrainingData/emnist-digits-train.csv", "MnistTrainingData/emnist-digits-test.csv"
+	// neuralnet.TrainNeuralNetwork(trainingDataFileName, 1, 500, 0.1, -1, -1, -1, 10, 10, weightSavingFileName)
+	// W1, b1, W2, b2 := neuralnet.ImportSavedWeights(weightSavingFileName)
+	// neuralnet.TestNeuralNet(testingDataFileName, -1, W1, b1, W2, b2)
+
+	weightSavingFileName, trainingDataFileName, testingDataFileName := "NeuralNetWeights/letterTestWeightsSaving_1.txt", "MnistTrainingData/emnist-byclass-train.csv", "MnistTrainingData/emnist-byclass-test.csv"
+	neuralnet.TrainNeuralNetwork(trainingDataFileName, 1, 500, 0.1, -1, -1, -1, 62, 62, weightSavingFileName)
 	W1, b1, W2, b2 := neuralnet.ImportSavedWeights(weightSavingFileName)
 	neuralnet.TestNeuralNet(testingDataFileName, -1, W1, b1, W2, b2)
 }
